f03be3f4cbb0f6215d8786870edc89681e51d8b4
amazoradi <amazoradi@gmail.com>
2018-12-07 08:51:51 -0600

    my hike and itinerary cards displaying on correct pages

--- src/components/itinerary/IntineraryList.js
+++ src/components/itinerary/IntineraryList.js
@@ -6,13 +6,43 @@ export default class Itinerary extends Component {
     hikes:[]
   }
 
-  getItineraryHikes = () => {
-    APIManager.getAllEntries("hikes", "/?completed=false" )
+ componentDidMount() {
+    const newState={}
+    APIManager.getAllEntries("hikes", "/?completed=false")
+      .then(hikes => newState.hikes = hikes)
+      .then(() => this.setState(newState))
   }
 
   render() {
     return (
-      <div>My Itinerary</div>
+      <div>
+      {
+        this.state.hikes.map(hike => 
+          <div key={hike.id}>
+            <img src={hike.imgSqSmall} atl={hike.imageUrl}></img>
+            <div className="cardText">
+              <h2>{hike.name}</h2>
+              <h4>{hike.location}</h4>
+              <h5>{hike.length} miles. {hike.stars} stars out of 5</h5>
+              <p>{hike.summary}</p> 
+              </div>
+              </div>
+              )
+      }
+      </div>
     )
   }
 }
+  
+
+// "userId": 1,
+//       "name": "Mount Mitchell - Black Mountain Crest Trail #179",
+//       "hikeLocation": "Burnsville, North Carolina",
+//       "length": 12.1,
+//       "stars": 12.1,
+//       "summary": "The highest, most airy ridge traverse in the east and a destination hike not to be taken lightly.",
+//       "completed": false,
+//       "public": false,
+//       "date_completed": "",
+//       "completed_message": "",
+//       "id": 3
--- src/components/myList/MyHikeList.js
+++ src/components/myList/MyHikeList.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import APIManager from "../../modules/APIManager"
 
 export default class MyHikeList extends Component {
-  state={
+  state = {
     hikes:[]
   }
 
-  getItineraryHikes = () => {
+  componentDidMount() {
+    const newState={}
     APIManager.getAllEntries("hikes", "/?completed=true")
+      .then(hikes => newState.hikes = hikes)
+      .then(() => this.setState(newState))
   }
 
   render() {
     return (
-      <div>My Hike List</div>
+      <div>
+        {
+          this.state.hikes.map(hike =>
+            <div key={hike.id}>
+              <img src={hike.imgSqSmall} atl={hike.imageUrl}></img>
+              <div className="cardText">
+                <h2>{hike.name}</h2>
+                <h4>{hike.location}</h4>
+                <h5>{hike.length} miles. {hike.stars} stars out of 5</h5>
+                <p>{hike.summary}</p>
+              </div>
+            </div>
+          )
+        }
+      </div>
     )
   }
 }
