865bd53708f070c25c4a2df6fbd78fadb1dfdeb2
amazoradi <amazoradi@gmail.com>
2018-12-06 21:48:51 -0600

    fixed registration form formating

--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, Label, Input } from 'reactstrap';
+// import { Button, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Checkbox, Form, Icon, Input } from 'semantic-ui-react'
 
 export default class Register extends Component {
 
@@ -8,25 +9,26 @@ export default class Register extends Component {
       <React.Fragment>
         <div className={this.props.hideLoginForm ? "loginForm" : "hide"}>
           <h2> Register Here </h2>
-          <FormGroup>
-            <Label for="registerName">Name</Label>
-            <Input type="text" name="name" id="registerName" placeholder="Your Name" onChange={this.props.handleFieldChange} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="registerEmail">Email</Label>
-            <Input type="email" name="email" id="registerEmail" placeholder="Email@email.com" onChange={this.props.handleFieldChange} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="registerPassword">Password</Label>
-            <Input type="password" name="password" id="registerPassword" placeholder="Password" onChange={this.props.handleFieldChange} />
-          </FormGroup>
-          <FormGroup check>
-            <Label for="registerLocation"> Your Location </Label>
-            <Input type="text" name="location" id="registerLocation" placeholder="You're current city" onChange={this.props.handleFieldChange} />
-
-          </FormGroup>
+          <Form>
+            <Form.Field>
+              <label for="registerName">Name</label>
+              <Input iconPosition='left' icon={<Icon name='user outline' />} type="text" placeholder='Joe Shepard' id="registerName" onChange={this.props.handleFieldChange} />
+            </Form.Field>
+            <Form.Field>
+              <label for="registerEmail">Email</label>
+              <Input iconPosition='left' icon={<Icon name='at' />} type="email" placeholder='Email@email.com' id="registerEmail" onChange={this.props.handleFieldChange} />
+            </Form.Field>
+            <Form.Field>
+              <label for="registerPassword">Password</label>
+              <Input iconPosition='left' icon={<Icon name='lock' />} type="password" placeholder='Password' id="registerPassword" onChange={this.props.handleFieldChange} />
+            </Form.Field>
+            <Form.Field check>
+              <label for="registerLocation">Your Location</label>
+              <Input iconPosition='left' icon={<Icon name='map outline' />} type="text" placeholder="You're current city" id="registerLocation" onChange={this.props.handleFieldChange} />
+            </Form.Field>
+          </Form>
           <Button onClick={() => this.props.handleRegister()}>Register</Button>
-          <button onClick={() => this.props.handleChangeForm()}>Return to login</button>
+          <Button onClick={() => this.props.handleChangeForm()}>Return to login</Button>
         </div>
       </React.Fragment>
     )
