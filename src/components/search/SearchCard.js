d1bcf39c838a2862869c927e09d7131e13e7929d
amazoradi <amazoradi@gmail.com>
2018-12-10 16:22:01 -0600

    user feedback added to adding hike buttons

--- src/components/search/SearchCard.js
+++ src/components/search/SearchCard.js
@@ -53,8 +53,14 @@ export default class SearchResultCard extends Component {
           <h5>{this.props.trail.length} miles. {this.props.trail.stars} stars out of 5</h5>
           <p>{this.props.trail.summary}</p>
           <div className="cardButtons">
-            <button className="btn" onClick={() => this.constructItineraryHikeCard()} >Add to my Itinerary</button>
-            <button className="btn" onClick={() => this.constructCompletedHikeCard()}>Add to My Hikes</button>
+            <button className="btn" onClick={() => {
+              this.constructItineraryHikeCard()
+              alert(`${this.props.trail.name} added to your itinerary. Happy hiking!`)
+            }} >Add to my Itinerary</button>
+            <button className="btn" onClick={() => {
+              this.constructCompletedHikeCard()
+              alert(`${this.props.trail.name} added your list of completed hikes. Well done!`)
+            }}>Add to My Hikes</button>
           </div>
         </div>
       </div>
